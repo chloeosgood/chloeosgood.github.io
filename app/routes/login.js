c9503f06d93978002cea3092edb794231ae70cfb
FlyingMongoose2018 <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-11-01 18:59:41 -0500

    big log in update
    Added the log in page. several node modules added. sessions and cookies are working. Sign up will come next

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,37 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-router.get('/login', function(req, res){
-    var MongoClient = require('mongodb').MongoClient;
-
-                var uri = "mongodb+srv://forum-admin:flyingmongooses@forum-cluster-main-pnxfd.mongodb.net/test?retryWrites=true";
-                MongoClient.connect(uri, function(err, client){
-                    //error checking
-                    if(err){
-                        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-                    }
-                    console.log('Connected...');;
-
-                   // perform actions on the collection object
-                    client.db("users").collection("user").find({}).toArray(function(err,result){
-                       if(err) throw err;
-                        console.log(result);
-                    });
-                   client.close();
-                });
-    res.render('Login',{
+router.get('/login', function (req, res) {
+    res.render('Login', {
         pageTitle: "Login",
         pageID: "Log in",
-        Location: "../"
+        Location: "../",
+        error: ""
     })
 });
 module.exports = router;
 
-router.get('/logout', function(req, res){
+router.post('/login', function (req, res, next) {
+    var uri = "mongodb+srv://forum-admin:flyingmongooses@forum-cluster-main-pnxfd.mongodb.net/test?retryWrites=true";
+    MongoClient.connect(uri, function (err, client) {
+        //error checking
+        if (err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+        }
+
+        // perform actions on the collection object
+        client.db("forum").collection("user").find({
+            'uname': req.body.username,
+            'psw': req.body.pass
+        }).toArray(function (err, result) {
+            if (err) throw err;
+            if (result.length == 0) {
+                res.render('Login', {
+                    pageTitle: "Login",
+                    pageID: "Log in",
+                    Location: "../",
+                    error: '*Username and password does not exist'
+                })
+            } else{
+                req.session.user = req.body.username;
+                console.log(req.cookie);
+                res.redirect('/');
+            }
+        });
+        client.close();
+    });
+    
     
 });
-
-
 
 module.exports = router;
