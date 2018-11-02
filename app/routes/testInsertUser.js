dd5e06f40193dfb812cb7421e32b0541f677df10
FlyingMongoose2018 <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-11-02 13:36:46 -0500

    Update app/routes/testInsertUser.js

--- app/routes/testInsertUser.js
+++ app/routes/testInsertUser.js
@@ -1,29 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 
-router.get('/test', function(req, res){
-    var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://forum-admin:flyingmongooses@forum-cluster-main-pnxfd.mongodb.net/test?retryWrites=true";
 
-                var uri = "mongodb+srv://forum-admin:flyingmongooses@forum-cluster-main-pnxfd.mongodb.net/test?retryWrites=true";
-                MongoClient.connect(uri, function(err, client){
-                    //error checking
-                    if(err){
-                        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-                    }
-                    console.log('Connected...');;
-
-                   // perform actions on the collection object
-                    client.db("forum").collection("user").insertOne({uname: TODO, scname: TODO, psw: TODO, email: TODO}){
-                       if(err) throw err;
-                        console.log(result);
-                        client.close();
-                    });
-                });
-    res.render('Login',{
-        pageTitle: "Login",
-        pageID: "Log in",
-        Location: "../"
-    })
+router.post('/testInsertUser', function(req, res, next){
+    
+    //TODO: add html vars for storing in db
+    var adduser = {
+        uname: TODO,
+        scname: TODO,
+        psw: TODO, 
+        email: TODO,
+        isAdmin: 0      //isAdmin set to 0 by default (not admin)
+    };
+    
+    MongoClient.connect(uri, function(err, client) {
+        assert.equal(null, err);
+        client.db('forum').collection('user').insertOne(adduser, function(err, result){
+            assert.equal(null, err);
+            console.log('User Inserted');
+            client.close();
+        });
+    });
+                        
+    res.redirect('/');            
 });
+
 module.exports = router;
