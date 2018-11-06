edce2b7b2e8883489aace363ed7ddfcb435f523b
FlyingMongoose2018 <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-11-06 17:14:32 -0600

    first ejs template
    look at index.js indexinfo.ejs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
     if (req.session.user) {
-    res.render('index', {
-        pageTitle: "Home",
-        pageID: "Home Page",
-        Location: "../"
-    })
-} else {
-res.redirect('login');
-}
+        res.render('index', {
+            pageTitle: "Home",
+            pageID: "Home Page",
+            Location: "../",
+            UserID: "15",
+            UserName: "Matt"
+        })
+    } else {
+        res.redirect('login');
+    }
 
 });
 
