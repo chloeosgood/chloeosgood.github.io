ae7b0335f02ccd6bff1734b6585465a24a15c178
FlyingMongoose2018 <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-11-08 19:43:54 -0600

    fixed log in and fixed main page
    simple fixes to the collisions and the main page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    if (true) {
+    if (req.session.user) {
         res.render('index', {
             pageTitle: "Home",
             pageID: "Home Page",
-            Location: "../"
-        })
+            Location: "../",
+            Username: req.session.user
+        });
+        console.log(req.session.user);
     } else {
         res.redirect('login');
     }
 
 });
-
-
 module.exports = router;
 //req.session.user
--- app/routes/thread.js
+++ app/routes/thread.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/Thread', function(req, res){
-    res.render('Thread',{
-        pageTitle: "Thread",
-        pageID: "Thread Page",
-        Location: "../"
-    })
+router.get('/Thread', function (req, res) {
+    if (req.session.user) {
+        res.render('Thread', {
+            pageTitle: "Thread",
+            pageID: "Thread Page",
+            Location: "../"
+        })
+    } else
+        res.redirect('/login');
 });
 module.exports = router;
