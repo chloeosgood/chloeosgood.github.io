0e749f7cecc269fa6b3e19eb027371cd0db5a3f4
Fay Jhou <Fay-Mac@FayJhou.local>
2018-11-17 18:26:18 -0600

    Updating recent fields

--- app/routes/thread.js
+++ app/routes/thread.js
@@ -1,19 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
+const Thread = require('../models/thread.js');
+const Post = require('../models/post.js');
+const Comment = require('../models/comment.js');
+
 router.get('/Thread', function (req, res) {
     if (req.session.user) {
-        res.render('Thread', {
-            pageTitle: "Thread",
-            pageID: "Thread Page",
-            Location: "../",
-            Username: req.session.user
-        })
+        Thread.list(function(err, threads) {
+            if (err) return next(err);
+
+            // thread.recentPost
+            // thread.recentUser
+            console.log(threads);
+            res.render('Thread', {
+                pageTitle: "Thread",
+                pageID: "Thread Page",
+                Location: "../",
+                Username: req.session.user,
+                Threads: threads
+            });
+        });
     } else
         {
-            req.session.redirect = '/Thread';
-           res.redirect('/login'); 
+            // req.session.redirect = '/Thread';
+            res.redirect('/login'); 
         }
         
 });
+
+router.get('/Thread/:thread/:postId', function(req, res, next) {
+    if (req.session.user) {
+        Post.findOne({ _id: req.params.postId }, function(err, post) {
+            if (err) return next(err);
+
+            res.render('Post', {
+                pageTitle: post.title,
+                pageID: `Post${post._id}`,
+                Location: "../",
+                Username: req.session.user,
+                Post: post
+            });
+        })
+    } else {
+        res.redirect('/login');
+    }
+});
+
+router.get('/Thread/:thread/:postId/addComment', function(req, res, next) {
+    if (req.session.user) {
+        // TO DO
+    } else {
+        res.redirect('/login');
+    }
+});
+
+router.post('/Thread/:thread/:postId/addComment', function(req, res, next) {
+    if (req.session.user) {
+        Comment.create({ user: req.session.user._id, post: req.params.postId, text: text }, function(err, comment) {
+            if (err) return next(err);
+
+            console.log(comment);
+
+        })
+    } else {
+        res.redirect('/login');
+    }
+});
+
 module.exports = router;
