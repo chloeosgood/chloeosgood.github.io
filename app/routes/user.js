b213ba26cf54568cb7e4ff795a3f55ebe281289d
Fay Jhou <Fay-Mac@FayJhou.local>
2018-11-19 09:25:56 -0600

    User's Info

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user.js');
+
 router.get('/User', function (req, res) {
     if (req.session.user) {
-        res.render('UserPage', {
-            pageTitle: "User",
-            pageID: "User Page",
-            Location: "../",
-            Username: req.session.user,
-            name: "Name",
-            major: "Major",
-            classification: "classification"
-        })
+        User.findOne({ username: req.session.user}, function(err, user) {
+            res.render('UserPage', {
+                pageTitle: "User",
+                pageID: "User Page",
+                Location: "../",
+                Username: req.session.user,
+                name: `${user.name.firstname} ${user.name.lastname}`,
+                major: user.major,
+                classification: user.classification
+            });
+        });
     } else
         {
             req.session.redirect = '/User';
