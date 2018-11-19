227f74178a2fb825fe9a14d2f22cdad5bb73b7f0
Fay Jhou <Fay-Mac@FayJhou.local>
2018-11-19 10:01:21 -0600

    Updating Index View

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const Thread = require('../models/thread.js');
+
 router.get('/', function (req, res, next) {
 
     if (req.session.user) {
-        res.render('index', {
-            pageTitle: "Home",
-            pageID: "Home Page",
-            Location: "../",
-            Username: req.session.user
+        Thread.list(function(err, threads) {
+            if (err) return next(err);
+            // thread.recentPost
+            // thread.recentUser
+            console.log(threads);
+            res.render('index', {
+                pageTitle: "Home",
+                pageID: "Home Page",
+                Location: "../",
+                Username: req.session.user,
+                Threads: threads
+            });
         });
     } else {
         res.redirect('login');
--- app/routes/thread.js
+++ app/routes/thread.js
@@ -5,16 +5,6 @@ const Thread = require('../models/thread.js');
 const Post = require('../models/post.js');
 const Comment = require('../models/comment.js');
 
-router.get('/test/thread', function(req, res, next) {
-    // Thread List, testing purpose, don't change else where.
-    Thread.list(function(err, threads) {
-        if (err) return next(err);
-        // thread.recentPost
-        // thread.recentUser
-        console.log(threads);
-    });
-});
-
 router.get('/Thread', function (req, res) {
     if (req.session.user) {
         Thread.list(function(err, threads) {
@@ -38,12 +28,12 @@ router.get('/Thread', function (req, res) {
         
 });
 
-router.get('/Thread/:thread/:postId', function(req, res, next) {
+router.get('/Thread/:threadName/:postId', function(req, res, next) {
     if (req.session.user) {
         Post.findOne({ _id: req.params.postId }, function(err, post) {
             if (err) return next(err);
 
-            Comment.list(function(err, comments) {
+            Comment.find({ post: post._id }, function(err, comments) {
                 if (err) return next(err);
 
                 res.render('Post', {
@@ -57,11 +47,12 @@ router.get('/Thread/:thread/:postId', function(req, res, next) {
             });
         });
     } else {
+        req.session.redirect = `/Thread/:${req.params.threadName}/:${post._id}`;
         res.redirect('/login');
     }
 });
 
-router.get('/Thread/:thread/:postId/addComment', function(req, res, next) {
+router.get('/Thread/:threadName/:postId/addComment', function(req, res, next) {
     if (req.session.user) {
         // TO DO
     } else {
@@ -69,7 +60,7 @@ router.get('/Thread/:thread/:postId/addComment', function(req, res, next) {
     }
 });
 
-router.post('/Thread/:thread/:postId/addComment', function(req, res, next) {
+router.post('/Thread/:threadName/:postId/addComment', function(req, res, next) {
     if (req.session.user) {
         User.findOne({ username: req.session.user }, function(err, user) {
             if (err) return next(err);
