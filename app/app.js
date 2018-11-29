36a61bdbd758a77fcac195746ffe365539a51192
FlyingMongoose2018 <44592622+FlyingMongoose2018@users.noreply.github.com>
2018-11-29 11:00:17 -0600

    404 error page
    fixed 404

--- app/app.js
+++ app/app.js
@@ -40,6 +40,16 @@ app.use(require('./routes/search'));
 
 require('./util/mongoose.js')(); //adds mongoose connection
 
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!');
+});
+
+
 var server = app.listen(5656, function(){
     console.log("Active");
 });
--- app/routes/user.js
+++ app/routes/user.js
@@ -4,14 +4,18 @@ var router = express.Router();
 const User = require('../models/user.js');
 const Post = require('../models/post.js');
 
-router.get('/User/:Username', function (req, res) {
+router.get('/User/:Username', function (req, res,next) {
     if(req.params.Username == "img_avatar4.png")
         return;
     if (req.session.user) {
         //i would like all posts with the given username also be passed through to the page in the
         //part that says posts. should just list all the posts.
         User.findOne({ username: req.params.Username}, function(err, user) {
+            if(err)
+                return next(err);
             Post.find( {user: user._id}, function(err, posts) {
+                if(err)
+                    return next(err);
                 //console.log(posts);
                 res.render('UserPage', {
                     pageTitle: "User",
